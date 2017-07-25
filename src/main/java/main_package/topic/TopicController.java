9dbcadba99f202ec59dd70c992aedc884a4bb5a6
David Velasquez <davelasquez12@gmail.com>
2017-07-25 03:04:11 -0500

    -Added getTopic(), addTopic(), updateTopic(), and deleteTopic() to the controller and the service.

--- src/main/java/main_package/topic/TopicController.java
+++ src/main/java/main_package/topic/TopicController.java
@@ -1,8 +1,7 @@
 package main_package.topic;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,5 +15,29 @@ public class TopicController
 	public List<Topic> getAllTopics()
 	{
 		return topicService.getAllTopics();
+	}
+
+	@RequestMapping("/topics/{id}")
+	public Topic getTopic(@PathVariable String id)
+	{
+		return topicService.getTopic(id);
+	}
+
+	@RequestMapping(method = RequestMethod.POST, value = "/topics")
+	public void addTopic(@RequestBody Topic topic)
+	{
+		topicService.addTopic(topic);
+	}
+
+	@RequestMapping(method = RequestMethod.PUT, value = "topics/{id}")
+	public void updateTopic(@RequestBody Topic topic, @PathVariable String id)
+	{
+		topicService.updateTopic(id, topic);
+	}
+
+	@RequestMapping(method = RequestMethod.DELETE, value = "topics/{id}")
+	public void deleteTopic(@PathVariable String id)
+	{
+		topicService.deleteTopic(id);
 	}
 }
--- src/main/java/main_package/topic/TopicService.java
+++ src/main/java/main_package/topic/TopicService.java
@@ -2,20 +2,48 @@ package main_package.topic;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class TopicService
 {
-	private List<Topic> topics = Arrays.asList(
+	private List<Topic> topics = new ArrayList<>(Arrays.asList(
 			new Topic("Spring", "Spring Framework", "Spring description"),
 			new Topic("Java", "Java Framework", "Java description"),
-			new Topic("Javascript", "Javascript Framework", "Javascript description")
+			new Topic("Javascript", "Javascript Framework", "Javascript description"))
 	);
 
 	public List<Topic> getAllTopics()
 	{
 		return topics;
+	}
+
+	public Topic getTopic(String id)
+	{
+		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+
+	public void addTopic(Topic topic)
+	{
+		topics.add(topic);
+	}
+
+	public void updateTopic(String id, Topic topic)
+	{
+		for(int i = 0; i < topics.size(); i++)
+		{
+			if(id.equals(topics.get(i).getId()))
+			{
+				topics.set(i, topic);
+				return;
+			}
+		}
+	}
+
+	public void deleteTopic(String id)
+	{
+		topics.removeIf(t -> t.getId().equals(id));
 	}
 }
